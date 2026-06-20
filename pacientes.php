c453d12bbde0b4236e34641893c70233082e8d4d
komputetoast <santieugesys@gmail.com>
2026-06-20 19:32:40 -0300

    cambio minimo

--- pacientes.php
+++ pacientes.php
@@ -4,8 +4,9 @@ echo "Hello World!";
 
 <!DOCTYPE html>
 <html>
-<body>
 
-<h1>Aplicacion para pacientes de APADEM, se agregara pronto</h1>
+<body>
+    <h1>Aplicacion para pacientes de APADEM, se agregara pronto</h1>
 </body>
+
 </html>
