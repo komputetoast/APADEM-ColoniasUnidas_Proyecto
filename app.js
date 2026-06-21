eece8a9ac7f0b4ad96fd7b64afe87e42b8f04c63
komputetoast <santieugesys@gmail.com>
2026-06-21 19:28:58 -0300

    Mejora mayor en las paginas, eliminacion del apartado de donaciones ya que se ha descartado

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const translations = {
   es: {
+    //Parte de arriba
     inicio: "Inicio",
     nosotros: "Sobre nosotros",
     transparencia: "Transparencia",
     donaciones1: "Donaciones",
+    pacientes: "Pacientes",
+    //Pagina web 1:
     servi0: "Nuestros Servicios",
     servi1: "Atención médica a cargo de especialistas",
     servi2: "Entrega de medicamentos",
@@ -16,15 +19,36 @@ const translations = {
     servi9: "Cirugías de ano imperforado",
     serviA: "Cirugías de labio leporino y fisura palatina",
     serviB: "Rehabilitación, estimulación temprana, ortodoncia, en el H.P.M.",
+    //Pagina web 2:
+    titulogros: "Nuestros logros",
+    tituloquienessomos:"¿Quienes somos?",
+    tituloquienessomos2:"¿Cuales son nuestros objetivos?",
+    tituloquienessomos3:"¿Cuales son nuestras prioridades?",
+    tituloquienessomos4:"Nuestras funciones",
+    acercadel1:"Somos una asociación civil sin fines de lucro, administrada por los propios padres y personas de buena voluntad, que no tienen carácter político, religioso o sectario alguno.",
+    acercadel2:"Promoción y reconocimiento de la existencia de impedidos mentales y físicos por medio de censos, encuestas y visitas.",
+    acercadel3:"Procurar los medios económicos, técnicos, médicos u otros para la atención de los impedidos físicos y mentales o mixtos o sea psicofísicos.",
+    acercadel4:"Atención a sordos y pago de estudios en el 'Instituto Antonio Próvolo' de la cuidad de Encarnación.",
+    acercadel5:"Obtener fondos para seguir las cirugías reparadoras de labios leporinos y Fisura Palatina.",
+    acercadel6:"Crear conciencia en la comunidad de que APADEM 'Colonias Unidas' debe tener Continuidad.",
+    acercadel7:"Buscar en todo momento el bienestar moral y espiritual del deficiente mental y de su familia.",
+    acercadel8:"Demostrar que los deficientes mentales, convenientemente tratados, pueden ser útiles a sí mismo, a su familia y a la sociedad.",
+    acercadel9:"Alentar la creación de instituciones y tratamientos especializados del paciente mental y físico.",
+    acercadelA:"Sensibilizar a la colectividad sobre la problemática de la deficiencia mental y física.",
+    acercadelB:"Asistir a los padres de hijos deficientes y físicos para el cumplimiento de su rol especial.",
+    //Parte de abajo
     descripcion:
       "Asociación de Padres y Amigos del Impedido Mental y Físico de las Colonias Unidas. Desde 1982. Asociación civil sin fines de lucro, administrada por los propios padres y personas de buena voluntad, que no tienen carácter político, religioso o sectario alguno.",
     contacto: "APADEM Colonias Unidas. Telefono: 071720524 – 0983 533111",
   },
   en: {
+    //Pagina de arriba
     inicio: "Start",
     nosotros: "About Us",
     transparencia: "Transparency",
     donaciones1: "Donations",
+    pacientes: "Patients",
+    //Pagina web 1:
     servi0: "Our services",
     servi1: "Special Medical Attention",
     servi2: "Medicine Delivery",
@@ -37,15 +61,36 @@ const translations = {
     servi9: "Imperforated anal surgery",
     serviA: "Cleft lip and palate surgeries",
     serviB: "Rehabilitation, early stimulation, orthodontics, at the MPH",
+    //Pagina web 2:
+    titulogros: "Our achievements",
+    tituloquienessomos:"About us",
+    tituloquienessomos2:"Our Goals",
+    tituloquienessomos3:"Our Priorities",
+    tituloquienessomos4:"Our functions",
+    acercadel1:"We are a nonprofit civil association managed by the parents themselves and people of goodwill, without any political, religious or sectarian character.",
+    acercadel2:"Promotion and recognition of the existence of mentally and physically disabled persons through censuses, surveys and visits.",
+    acercadel3:"To obtain economic, technical, medical or other means for the care of physically and mentally disabled or mixed, i.e. psychophysical, persons.",
+    acercadel4:"Care for the deaf and payment of studies at the 'Instituto Antonio Próvolo' in the city of Encarnación.",
+    acercadel5:"To obtain funds to continue restorative surgeries for cleft lips and palates.",
+    acercadel6:"To raise community awareness that APADEM 'Colonias Unidas' must continue.",
+    acercadel7:"To always seek the moral and spiritual wellbeing of the mentally disabled person and their family.",
+    acercadel8:"To demonstrate that mentally disabled people, when properly treated, can be useful to themselves, their family and society.",
+    acercadel9:"To encourage the creation of institutions and specialized treatments for mental and physical patients.",
+    acercadelA:"To raise public awareness about the problems of mental and physical disability.",
+    acercadelB:"To assist the parents of disabled children in fulfilling their special role.",
+    //Parte de abajo
     descripcion:
       "APADEM. Parents and Friends Association of Mental and Physical Handicap of Colonias Unidas. Established since 1982. Managed by our good volunteers and parents with no political, religious or sectary character.",
     contacto: "APADEM Colonias Unidas. Phone Number: 071720524 – 0983 533111",
   },
     de: {
+    //Pagina de arriba
     inicio: "Start",
     nosotros: "Über uns",
     transparencia: "Transparenz",
     donaciones1: "Spenden",
+    pacientes: "Patients",
+    //Pagina Web 1:
     servi0: "Unsere Leistungen",
     servi1: "Spezialmedizinische Versorgung",
     servi2: "Kostenübernahme durch spezielle Einrichtungen",
@@ -58,15 +103,35 @@ const translations = {
     servi9: "Operationen bei Lippen- und Gaumenspalten",
     serviA: "Medikamentenlieferung",
     serviB: "Rehabilitation, Frühförderung, Kieferorthopädie im MPH (Medizinisches Kinderkrankenhaus)",
+    //Pagina Web 2:
+    titulogros: "Unsere Erfolge",
+    tituloquienessomos:"Über uns",
+    tituloquienessomos2:"Unsere Ziele",
+    tituloquienessomos3:"Unsere Prioritäten",
+    tituloquienessomos4:"Unsere Aufgaben",
+    acercadel1:"Wir sind ein gemeinnütziger Verein, der von den Eltern selbst und gutwilligen Menschen geleitet wird und keinen politischen, religiösen oder sektiererischen Charakter hat.",
+    acercadel2:"Förderung und Anerkennung der Existenz geistig und körperlich behinderter Menschen durch Volkszählungen, Umfragen und Besuche.",
+    acercadel3:"Bereitstellung wirtschaftlicher, technischer, medizinischer oder anderer Mittel zur Betreuung von körperlich und geistig behinderten oder gemischten, also psychophysischen Personen.",
+    acercadel4:"Betreuung von Gehörlosen und Bezahlung von Studien am 'Instituto Antonio Próvolo' in der Stadt Encarnación.",
+    acercadel5:"Beschaffung von Mitteln, um die rekonstruktiven Operationen bei Lippen-Kiefer-Gaumenspalten fortzusetzen.",
+    acercadel6:"Bewusstsein in der Gemeinschaft schaffen, dass APADEM 'Colonias Unidas' fortgeführt werden muss.",
+    acercadel7:"Stets das moralische und spirituelle Wohl des geistig behinderten Menschen und seiner Familie zu suchen.",
+    acercadel8:"Zu zeigen, dass geistig behinderte Menschen, wenn sie angemessen behandelt werden, für sich, ihre Familie und die Gesellschaft nützlich sein können.",
+    acercadel9:"Die Schaffung von Einrichtungen und spezialisierten Behandlungen für geistig und körperlich behinderte Patienten zu fördern.",
+    acercadelA:"Die Öffentlichkeit für die Problematik geistiger und körperlicher Behinderung zu sensibilisieren.",
+    acercadelB:"Eltern behinderter Kinder bei der Erfüllung ihrer besonderen Rolle zu unterstützen.",
     descripcion:
       "APADEM. Eltern- und Freundeskreis für Menschen mit geistiger und körperlicher Behinderung in Colonias Unidas. Gegründet im Jahr 1982. Der Verein wird von unseren engagierten ehrenamtlichen Helfern und Eltern geleitet und ist weder politisch noch religiös oder sektenhaft ausgerichtet.",
     contacto: "APADEM Colonias Unidas. Telefonnummer: 071720524 – 0983 533111",
     },
     ja: {
+    //Pagina de arriba
     inicio: "ホーム",
     nosotros: "当団体について",
     transparencia: "透明性",
     donaciones1: "寄付",
+    pacientes: "患者",
+    //Pagina Web 1:
     servi0: "当社のサービス",
     servi1: "特別な医療ケア",
     servi2: "医薬品の配達",
@@ -79,6 +144,24 @@ const translations = {
     servi9: "肛門閉鎖症の手術",
     serviA: "口唇口蓋裂の手術",
     serviB: "MPH（小児医療病院）におけるリハビリテーション、早期刺激療法、矯正歯科治療",
+    //Pagina Web 2:
+    titulogros: "私たちの実績",
+    tituloquienessomos:"私たちについて",
+    tituloquienessomos2:"私たちの目標",
+    tituloquienessomos3:"私たちの優先事項",
+    tituloquienessomos4:"私たちの役割",
+    acercadel1:"私たちは、保護者自身と善意ある人々によって運営される非営利の市民団体であり、政治的、宗教的、または宗派的な性格を一切持ちません。",
+    acercadel2:"国勢調査、アンケート、訪問を通じて、知的障がいおよび身体障がい者の存在を促進し認識すること。",
+    acercadel3:"身体的および知的、あるいは混合、すなわち心理身体的障がい者のケアのために、経済的、技術的、医療的またはその他の手段を確保すること。",
+    acercadel4:"聴覚障がい者のケアと、エンカルナシオン市の『インスティトゥート・アントニオ・プロボロ』での学費の支払い。",
+    acercadel5:"口唇口蓋裂の修復手術を継続するための資金を調達すること。",
+    acercadel6:"APADEM『コロニアス・ウニダス』が継続されるべきであることを地域社会に認識させること。",
+    acercadel7:"知的障がい者とその家族の道徳的・精神的な福祉を常に追求すること。",
+    acercadel8:"適切に治療されれば、知的障がい者は自分自身や家族、社会に役立つことができることを示すこと。",
+    acercadel9:"精神的および身体的な患者のための専門的な施設と治療の創設を促すこと。",
+    acercadelA:"精神的および身体的障がいの問題について社会の意識を高めること。",
+    acercadelB:"障がいのある子どもを持つ親がその特別な役割を果たせるよう支援すること。",
+    //Parte de abajo
     descripcion:
       "コロニアス・ウニダスにおける知的・身体障がい者の保護者・支援者協会（APADEM）。1982年設立。非営利の市民団体であり、保護者や善意ある人々自身によって運営されており、政治的、宗教的、あるいは宗派的な性格を一切有していない。",
     contacto: "APADEM Colonias Unidas. 電話番号: 071720524 – 0983 533111",
@@ -93,7 +176,7 @@ function changeLanguage(lang) {
   // Update the HTML lang attribute for accessibility/SEO
   htmlRoot.setAttribute("lang", lang);
 
-  // Find all elements with a data-key attribute
+  // Encontrar todos los elementos con el atributo "data-key"
   document.querySelectorAll("[data-key]").forEach((element) => {
     const key = element.getAttribute("data-key");
     if (translations[lang] && translations[lang][key]) {
@@ -101,7 +184,7 @@ function changeLanguage(lang) {
     }
   });
 
-  // Save selection to local storage
+  // Guardar la selección al almacenamiento local
   localStorage.setItem("preferredLang", lang);
 }
 
